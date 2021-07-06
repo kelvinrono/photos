7377998a11e8f893e0f7f02c976c6d275e7f627a
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-07 02:14:05 +0300

    create all the models

--- images/models.py
+++ images/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField 
 
-# Create your models here.
+class Gallery(models.Model):
+    id=models.IntegerField( primary_key=True)
+    title = models.CharField(max_length=55)
+    description = models.TextField(max_length=100)
+    image = CloudinaryField('image') 
+    pub_date = models.DateTimeField(auto_now_add=True)
+    category =  models.ForeignKey('Category',on_delete=models.CASCADE)
+    location =  models.ForeignKey('Location',on_delete=models.CASCADE)
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        image_category = cls.objects.filter(title__icontains=search_term)
+        return image_category
+
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        ordering = ['title']
+
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def get_image(cls):
+        image= cls.objects.get(pk=id)
+        return image
+
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+class Category(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+
